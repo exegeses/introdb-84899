f035985a1ddfddcd7d6d25c5889b6ed71935fcd5
Marcos <exegeses@gmail.com>
2026-03-20 10:11:03 -0300

    Concepto de Foreign keys y Cardinalidad

---
+++ clase5/borrar-registros.md
@@ -0,0 +1,15 @@
+# Eliminar registros
+ > Para eliminar registros dentro de una tabla utilizamos el comando **DELETE**
+ > Debemos especificar mediante un filtro cuál es el registro que queremos eliminar 
+
+> Sintaxis:
+
+    DELETE FROM tabla
+      WHERE columnaPrimaryKey = valorPK;
+
+> Ejemplo:
+
+    DELETE FROM proveedores  
+       WHERE idProveedor = 15;  
+
+> ver: <https://youtu.be/i_cVJgIz_Cs?si=FZoJHXM4dj-fzult>
---
+++ clase5/tablas-catalogo.sql
@@ -0,0 +1,26 @@
+# creación de tablas proyecto catálogo
+create table marcas
+(
+    idMarca smallint unsigned auto_increment primary key,
+    marca varchar(40) unique not null
+);
+
+create table categorias
+(
+    idCategoria smallint unsigned auto_increment primary key,
+    categoria varchar(40) unique not null
+);
+
+create table productos
+(
+    idProducto mediumint unsigned auto_increment primary key,
+    productos varchar(50) unique not null,
+    precio decimal(10,2) not null,
+    idMarca smallint unsigned not null,
+    idCategoria smallint unsigned not null,
+    description varchar(1000) not null,
+    imagen varchar(50) not null,
+    activo boolean not null,
+    foreign key (idMarca) references marcas (idMarca),
+    foreign key (idCategoria) references categorias (idCategoria)
+);
